1e9cf01ea559051ebe1cdac938dbd88b5be43973
Xiaolin Liu <liuxiaolinfk@gmail.com>
2025-05-13 15:09:08 -0500

    Add input files in quench

--- quench/quench.sh
+++ quench/quench.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-#SBATCH -A m697 
-#SBATCH -C gpu
-#SBATCH -q regular
-#SBATCH -N 1
-#SBATCH --gpu-bind=none
-#SBATCH -t 48:00:00
-#SBATCH --job-name="iScale"
-#SBATCH --mail-user=xiaolin.liu@vanderbilt.edu
-#SBATCH --mail-type=FAIL
+#SBATCH --time=144:00:00
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH -J test
+#SBATCH -o slurm_%j.out
+#SBATCH -e slurm_%j.err
+module load VASP
+vasp_exe=`which vasp_gam`
+echo "cuda:$CUDA_VISIBLE_DEVICES"
 
-module load vasp/6.4.3-gpu
-# One can use up to 16 OpenMP threads-per-MPI-rank when using
-#  4 GPUs-per-node.
-export OMP_NUM_THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
-
-VASP_COMMAND="srun -n 4 -c 32 -G 4 --cpu-bind=cores --gpu-bind=none vasp_gam"
+VASP_COMMAND="mpirun -np ${SLURM_NTASKS} $vasp_exe"
 
 startTemp=1500
 endTemp=300
