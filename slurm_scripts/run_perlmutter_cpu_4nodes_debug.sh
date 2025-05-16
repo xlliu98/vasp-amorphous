0ef21220da7f867d13ef74421148dcc7e38ecaac
Xiaolin Liu <liuxiaolinfk@gmail.com>
2025-05-16 12:46:49 -0700

    Add perlmutter scripts

---
+++ slurm_scripts/run_perlmutter_cpu_4nodes_debug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=16
+#SBATCH -C cpu
+#SBATCH -q debug
+#SBATCH -J AIMD
+#SBATCH --mail-user=xiaolin.liu@vanderbilt.edu
+#SBATCH --mail-type=FAIL
+#SBATCH -t 00:30:00
+module load vasp/6.4.3-cpu
+#OpenMP settings:
+export OMP_NUM_THREADS=8
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+VASP_COMMAND="srun -n ${SLURM_NTASKS} -c  ${SLURM_CPUS_PER_TASK} --cpu_bind=cores vasp_gam"
