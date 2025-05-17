989c72c61ba939f9db960f09aa20a890127ea281
Xiaolin Liu <liuxiaolinfk@gmail.com>
2025-05-17 13:07:35 -0400

    Fixed a typo

--- helper_scripts/mlff_validation_runML.sh
+++ helper_scripts/mlff_validation_runML.sh
@@ -3,7 +3,7 @@ cd POSCAR_for_validation/
 
 for i in {0..49}; do
    echo "test configuration " $i
-   cd ab_${i}
+   cd ML_${i}
    ln -sf ../../../../../INCAR_SP_ML INCAR
    ln -sf ../../../../ML_FF ML_FF
    ln -sf ../../../../../POTCAR POTCAR
